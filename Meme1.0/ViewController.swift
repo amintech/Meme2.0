3fe8635928901cc4a4c84436c90f8a7d45ff14ef
Amin Saleh <AminSaleh@macbook-air-alkhas-b-aminsaleh.local>
2018-11-05 10:46:32 +0300

    Initial Commit

---
+++ Meme1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by  AminSaleh on 27/02/1440 AH.
+//  Copyright © 1440 AminTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
